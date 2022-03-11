0f57083be2410cc0daf36a7163603d2df9ef9d61
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2022-03-11 16:45:12 +0700

    Update ApiResponse.php

--- app/Http/Controllers/ApiResponse.php
+++ app/Http/Controllers/ApiResponse.php
@@ -1,21 +1,26 @@
 <?php
+
 namespace App\Http\Controllers;
 
-trait ApiResponse {
-    private function responseSuccess($data = null, $message = 'success')
+trait ApiResponse
+{
+    private function responseSuccess($data = null, $message = 'success', $statusCode = 200): \Illuminate\Http\JsonResponse
     {
         return response()->json([
             'status' => true,
+            'data' => $data,
+            'errors' => null,
             'message' => $message,
-            'data' => $data
-        ]);
+        ], $statusCode);
     }
-    private function responseError($data = null, $message = 'error')
+
+    private function responseError($errors = null, $message = null, $statusCode = 400): \Illuminate\Http\JsonResponse
     {
         return response()->json([
             'status' => false,
+            'data' => null,
+            'errors' => $errors,
             'message' => $message,
-            'data' => $data
-        ]);
+        ], $statusCode);
     }
 }
